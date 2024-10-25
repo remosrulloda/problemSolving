d894d5039d4a31806548639d4e5e67552c3c1297
Remo <remosrulloda@gmail.com>
2024-10-24 23:15:42 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+class ListNode:
+    def __init__(self, val = 0, left = None, right = None):
+        self.val = val
+        self.right = right
+        self.left = left
+
+class Solution:
+    def pyramid(self, head: ListNode):
+        
+
+
